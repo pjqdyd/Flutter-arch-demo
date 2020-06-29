61011629e55993d3fc5d7d8e1e9627685de1c3a3
pjqdyd <1909025079@qq.com>
2020-06-29 13:42:57 +0800

    update

--- lib/provider/ProviderWidget.dart
+++ lib/provider/ProviderWidget.dart
@@ -2,6 +2,10 @@ import 'package:flutter/cupertino.dart';
 import 'package:provider/provider.dart';
 
 ///封装ProviderWidget, T viewModel是要监听的VM视图数据模型
+///这个封装表示把传入的T viewModel:
+/// 提供初始化时的一个onReady方法
+/// 包裹进ChangeNotifierProvider组件, 使得该viewModel可以监听到notifyListeners();产生的变化
+/// 并用Consumer消费监听, 回调传入的builder函数方法获取更新的viewModel数据
 class ProviderWidget<T extends ChangeNotifier> extends StatefulWidget{
 
   final Widget Function(BuildContext context, T value, Widget child) builder;
@@ -39,6 +43,12 @@ class _ProviderWidgetState<T extends ChangeNotifier> extends State<ProviderWidge
           child: widget.child,
         ),
     );
+    // Consumer<T>( //等同与上面
+    //   builder: (context, viewModel, child){ //消费者提供的方法
+    //     return widget.builder(context, viewModel, child); //回调builder方法
+    //   },
+    //   child: widget.child,
+    // );
   }
 
 }
